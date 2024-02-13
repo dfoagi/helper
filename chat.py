eb7e7b87140d4e7769b70abea3fbf6918ace9387
dfoagi <44113570+dfoagi@users.noreply.github.com>
2024-02-14 04:29:35 +0700

    first

---
+++ chat.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+import requests
+
+url = "http://ip-31-222-229-137-89923.vps.hosted-by-mvps.net:9090/"
+
+sg.theme('GreenTan')
+
+layout = [[sg.Text('Your output will go here', size=(40, 1))],
+          [sg.Output(size=(110, 20), font=('Helvetica 10'))],
+          [sg.Multiline(size=(70, 5), enter_submits=True, key='-QUERY-', do_not_clear=False),
+           sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0]), bind_return_key=True),
+           sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
+
+window = sg.Window('Chat window', layout, font=('Helvetica', ' 13'), default_button_element_size=(8,2), use_default_focus=False)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'EXIT'):
+        break
+    if event == 'SEND':
+        query = values['-QUERY-'].rstrip()
+
+        print('User: {}'.format(query), flush=True)
+        r = requests.post(url, json={"question": query})
+        print('Helper: ', r.json()['answer'])
+
+window.close()
+
---
+++ main.py
@@ -0,0 +1,28 @@
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import keyboard
+
+model = Model("vosk-model-small-ru-0.22")
+recognizer = KaldiRecognizer(model, 16000)
+
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+while True:
+    data = stream.read(4096)
+    if recognizer.AcceptWaveform(data):
+        text = recognizer.Result()
+        print(text)
+
+# while True:
+#     question = []
+#     while keyboard.read_key() == "q":
+#         data = stream.read(4096, exception_on_overflow=False)
+#         if recognizer.AcceptWaveform(data):
+#             text = recognizer.Result()
+#             question.append(text[14:-3])
+#             print(question)
+#     if question:
+#         print(' '.join(question))
+#         question = []
